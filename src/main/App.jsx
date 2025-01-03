2371c9c9b3053fecbcee980a8f944659798736c1
DavidIsrael <davidisrael.rj@gmail.com>
2025-01-03 20:57:37 -0300

    Ajustando o projeto

--- src/main/App.jsx
+++ src/main/App.jsx
@@ -4,18 +4,10 @@ import './App.css'
 
 import React from 'react'
 import { BrowserRouter } from 'react-router-dom'
-import Routes from './Routes'
-
-import Footer from '../components/template/Footer'
-import Logo from '../components/template/Logo'
-import Nav from '../components/template/Nav'
 
 export default props =>
     <BrowserRouter>
         <div className="app">
-            <Logo />
-            <Nav />
-            <Routes/>
-            <Footer />
+            <h1>Mercados</h1>
         </div>
     </BrowserRouter>
