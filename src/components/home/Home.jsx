b54b059c29e20fba2eb2c03cf87b7465164c4a36
DavidIsrael <davidisrael.rj@gmail.com>
2025-01-07 10:35:14 -0300

    Componente Home

---
+++ src/components/home/Home.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import Main from "../template/Main";
+
+export default props =>
+    <Main icon="home" title="Início"
+subtitle="Mercado Preço da Promoção">
+<div className="display-4">Bem Vindo!</div>
+<hr />
+<p className="mb-0">Mercado Preço da Promoção</p>
+</Main>
--- src/main/App.jsx
+++ src/main/App.jsx
@@ -6,16 +6,15 @@ import React from 'react'
 import { BrowserRouter } from 'react-router-dom'
 import Logo from '../components/template/Logo'
 import Nav from '../components/template/Nav'
-import Main from '../components/template/Main'
 import Footer from '../components/template/Footer'
+import Home from '../components/home/Home'
 
 export default props =>
     <BrowserRouter>
         <div className="app">
-            <Logo/>
-            <Nav/>
-            <Main icon="home" title="Início"
-                subtitle="Mercado Preço da Promoção"/>
-            <Footer/>
+            <Logo />
+            <Nav />
+            <Home />
+            <Footer />
         </div>
     </BrowserRouter>
