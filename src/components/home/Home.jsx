773e2211fc664e9e50cfbd11e521384af44e263c
DavidIsrael <davidisrael.rj@gmail.com>
2025-01-08 18:26:20 -0300

    Cadastro de Usuário #1

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -3,8 +3,8 @@ import Main from "../template/Main";
 
 export default props =>
     <Main icon="home" title="Início"
-subtitle="Mercado Preço da Promoção">
-<div className="display-4">Bem Vindo!</div>
-<hr />
-<p className="mb-0">Mercado Preço da Promoção</p>
-</Main>
+        subtitle="Mercado Preço da Promoção">
+        <div className="display-4">Bem Vindo!</div>
+        <hr />
+        <p className="mb-0">Mercado Preço da Promoção</p>
+    </Main>
