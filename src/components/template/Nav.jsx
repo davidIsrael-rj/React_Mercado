0f5c365a04db6fc089701b403c1535e452edabfa
DavidIsrael <davidisrael.rj@gmail.com>
2025-01-09 16:41:50 -0300

    colocando logo no componente Nav

--- src/components/template/Nav.jsx
+++ src/components/template/Nav.jsx
@@ -1,4 +1,5 @@
 import { Link } from 'react-router-dom'
+import logo from '../../assets/imgs/img.jpg'
 import './Nav.css'
 import React from 'react'
 
@@ -6,9 +7,16 @@ export default props =>
     <aside className="menu-area">
         <nav className="menu">
             <Link to="/">
+                <img className="foto" src={logo} alt="logos" />
+                <p>Ola</p>
+            </Link>
+            <Link to="/">
                 <i className="fa fa-home"></i>Início
             </Link>
-            <Link to="/users"> 
+            <Link to="/">
+                <i className="fa fa-home"></i>Início
+            </Link>
+            <Link to="/users">
                 <i className="fa fa-users"></i>Usuários
             </Link>
         </nav>
