84d315ae11f8563b82dd95753c0ebd521bd56fef
DavidIsrael <davidisrael.rj@gmail.com>
2025-01-12 22:16:09 -0300

    função save

--- src/components/user/UserCrud.jsx
+++ src/components/user/UserCrud.jsx
@@ -22,7 +22,18 @@ export default class UserCrud extends Component {
         this.setState({user: initialState.user})
     }
 
-   
+    save(){
+        const user = this.state.user
+        const method = user.id ? 'put' : 'post'
+        const url = user.id ? `${baseUrl}/${user.id}` : baseUrl
+        axios[method](url, user)
+            .then(resp =>{
+              const list = this.getUpdatedList(resp.data)
+              this.setState({user:initialState.user, list}) 
+            })
+    }
+
+  
 
     render(){
         return(
