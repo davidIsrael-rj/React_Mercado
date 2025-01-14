8c5cdf34ab555ed86440a900e9ba57ae750a853c
DavidIsrael <davidisrael.rj@gmail.com>
2025-01-13 21:30:27 -0300

    função updateField

--- src/components/user/UserCrud.jsx
+++ src/components/user/UserCrud.jsx
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import Main from "../template/Main";
 import axios from "axios";
 
-const headerProps ={
+const headerProps = {
     icon: 'users',
     title: 'Usuários',
     subtitle: 'Cadastro de Usuários, Incluir, Listar, Alterar e Excluir! '
@@ -10,37 +10,43 @@ const headerProps ={
 
 const baseUrl = 'http://localhost:3001/users'
 const initialState = {
-    user:{name:'', email:''},
-    list:[]
+    user: { name: '', email: '' },
+    list: []
 }
 
 export default class UserCrud extends Component {
 
-    state={...initialState}
+    state = { ...initialState }
 
-    clear(){
-        this.setState({user: initialState.user})
+    clear() {
+        this.setState({ user: initialState.user })
     }
 
-    save(){
+    save() {
         const user = this.state.user
         const method = user.id ? 'put' : 'post'
         const url = user.id ? `${baseUrl}/${user.id}` : baseUrl
         axios[method](url, user)
-            .then(resp =>{
-              const list = this.getUpdatedList(resp.data)
-              this.setState({user:initialState.user, list}) 
+            .then(resp => {
+                const list = this.getUpdatedList(resp.data)
+                this.setState({ user: initialState.user, list })
             })
     }
 
-    getUpdatedList(user){
+    getUpdatedList(user) {
         const list = this.state.list.filter(u => u.id !== user.id)
         list.unshift(user)
         return list
     }
 
-    render(){
-        return(
+    updateField(event) {
+        const user = { ...this.state.user }
+        user[event.target.name] = event.target.value
+        this.setState({ user })
+    }
+
+    render() {
+        return (
             <Main {...headerProps}>
                 Cadastro de Usuário
             </Main>
