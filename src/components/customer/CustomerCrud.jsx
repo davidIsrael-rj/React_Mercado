8793090d89d3c399cfab5eb41e6a118f85736862
DavidIsrael <davidisrael.rj@gmail.com>
2025-01-17 21:35:36 -0300

    Tabela com os Clientes

--- src/components/customer/CustomerCrud.jsx
+++ src/components/customer/CustomerCrud.jsx
@@ -41,10 +41,54 @@ export default class CustomerCrud extends Component {
             })
     }
 
+    renderTable(){
+        return(
+            <table className="table mt-4">
+                <thead>
+                    <tr>
+                        <th>Id</th>
+                        <th>Nome</th>
+                        <th>Endereço</th>
+                        <th>N°</th>
+                        <th>Bairro</th>
+                        <th>Estado</th>
+                        <th>CEP</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {this.renderRows()}
+                </tbody>
+            </table>
+        )
+    }
+
+    renderRows(){
+        return this.state.list.map(customer => {
+            return (
+                <tr key={customer.id}>
+                    <td>{customer.id}</td>
+                    <td>{customer.name}</td>
+                    <td>{customer.endereco}</td>
+                    <td>{customer.numero}</td>
+                    <td>{customer.bairro}</td>
+                    <td>{customer.estado}</td>
+                    <td>{customer.cep}</td>
+                    <td>
+                        <button className="btn btn-warning">
+                            <i className="fa fa-pencil"></i>
+                        </button>
+                        <button className="btn btn-danger ml-2">
+                            <i className="fa fa-trash"></i>
+                        </button>
+                    </td>
+                </tr>
+            )
+        })
+    }
     render() {
         return (
             <Main {...headerProps}>
-                Clientes
+                {this.renderTable()}
             </Main>
         )
     }
