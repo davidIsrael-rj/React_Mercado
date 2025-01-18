f76cef6fa301c68eb3be012f469803615613c1c7
DavidIsrael <davidisrael.rj@gmail.com>
2025-01-17 21:10:45 -0300

    função clear

--- src/components/customer/CustomerCrud.jsx
+++ src/components/customer/CustomerCrud.jsx
@@ -4,15 +4,33 @@ import Main from "../template/Main";
 const headerProps = {
     icon: 'shopping-cart',
     title: 'Clientes',
-    subtitle:'Cadastro de Clientes, Incluir, Listar, Alterar e Excluir!'
+    subtitle: 'Cadastro de Clientes, Incluir, Listar, Alterar e Excluir!'
+}
+
+const baseUrl = 'http://localhost:3001/customer'
+const initialState = {
+    customer: { name: '', telefone: '', endereco: '', numero: '', bairro: '', cidade: '', estado: '', cpf: '', rg: '', cep: '' },
+    list: []
 }
 
 
 
-export default class CustomerCrud extends Component{
-   
-    render(){
-        return(
+export default class CustomerCrud extends Component {
+
+    state = { ...initialState }
+
+    componentWillMount() {
+        axios(baseUrl).then(resp => {
+            this.setState({ list: resp.data })
+        })
+    }
+
+    clear(){
+        this.setState({customer: initialState.customer})
+    }
+
+    render() {
+        return (
             <Main {...headerProps}>
                 Clientes
             </Main>
