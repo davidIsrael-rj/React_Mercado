ca6d81e41fc4b5e5004ded4a86ec8cfe8aca52bc
DavidIsrael <davidisrael.rj@gmail.com>
2025-01-17 22:07:05 -0300

    formulário de cliente

--- src/components/customer/CustomerCrud.jsx
+++ src/components/customer/CustomerCrud.jsx
@@ -47,11 +47,155 @@ export default class CustomerCrud extends Component {
         return list
     }
 
-    updateField(event){
-        const customer = {...this.state.customer}
-        customer[event.targer.name] = event.target.value
+    updateField(event) {
+        const customer = { ...this.state.customer }
+        customer[event.target.name] = event.target.value
+        this.setState({ customer })
+    }
+
+    load(customer){
         this.setState({customer})
     }
+
+    remove(customer){
+        axios.delete(`${baseUrl}/${customer.id}`).then(resp =>{
+            const list = this.getUpdatedList(customer, false)
+            this.setState({list})
+        })
+    }
+
+    renderForm() {
+        return (
+            <div className="form">
+                <div className="row">
+                    <div className="col-12 col-md-6">
+                        <div className="form-group">
+                            <label>Nome</label>
+                            <input type="text" className="form-control"
+                                name="name"
+                                value={this.state.customer.name}
+                                onChange={e => this.updateField(e)}
+                                placeholder="Digite o nome..."
+                            />
+                        </div>
+                    </div>
+                    <div className="col-12 col-md-6">
+                        <div className="form-group">
+                            <label>Telefone</label>
+                            <input type="text" className="form-control"
+                                name="telefone"
+                                value={this.state.customer.telefone}
+                                onChange={e => this.updateField(e)}
+                                placeholder="Digite o telefone..."
+                            />
+                        </div>
+                    </div>
+                    <div className="col-12 col-md-6">
+                        <div className="form-group">
+                            <label>Endereco</label>
+                            <input type="text" className="form-control"
+                                name="endereco"
+                                value={this.state.customer.endereco}
+                                onChange={e => this.updateField(e)}
+                                placeholder="Digite o endereço..."
+                            />
+                        </div>
+                    </div>
+                    <div className="col-12 col-md-6">
+                        <div className="form-group">
+                            <label>Numero</label>
+                            <input type="text" className="form-control"
+                                name="numero"
+                                value={this.state.customer.numero}
+                                onChange={e => this.updateField(e)}
+                                placeholder="Digite o numero..."
+                            />
+                        </div>
+                    </div>
+                    <div className="col-12 col-md-6">
+                        <div className="form-group">
+                            <label>Bairro</label>
+                            <input type="text" className="form-control"
+                                name="bairro"
+                                value={this.state.customer.bairro}
+                                onChange={e => this.updateField(e)}
+                                placeholder="Digite o bairro..."
+                            />
+                        </div>
+                    </div>
+                    <div className="col-12 col-md-6">
+                        <div className="form-group">
+                            <label>Cidade</label>
+                            <input type="text" className="form-control"
+                                name="cidade"
+                                value={this.state.customer.cidade}
+                                onChange={e => this.updateField(e)}
+                                placeholder="Digite a cidade..."
+                            />
+                        </div>
+                    </div>
+                    <div className="col-12 col-md-6">
+                        <div className="form-group">
+                            <label>Estado</label>
+                            <input type="text" className="form-control"
+                                name="estado"
+                                value={this.state.customer.estado}
+                                onChange={e => this.updateField(e)}
+                                placeholder="Digite o estado..."
+                            />
+                        </div>
+                    </div>
+                    <div className="col-12 col-md-6">
+                        <div className="form-group">
+                            <label>CPF</label>
+                            <input type="text" className="form-control"
+                                name="cpf"
+                                value={this.state.customer.cpf}
+                                onChange={e => this.updateField(e)}
+                                placeholder="Digite o cpf..."
+                            />
+                        </div>
+                    </div>
+                    <div className="col-12 col-md-6">
+                        <div className="form-group">
+                            <label>RG</label>
+                            <input type="text" className="form-control"
+                                name="rg"
+                                value={this.state.customer.rg}
+                                onChange={e => this.updateField(e)}
+                                placeholder="Digite o RG..."
+                            />
+                        </div>
+                    </div>
+                    <div className="col-12 col-md-6">
+                        <div className="form-group">
+                            <label>CEP</label>
+                            <input type="text" className="form-control"
+                                name="cep"
+                                value={this.state.customer.cep}
+                                onChange={e => this.updateField(e)}
+                                placeholder="Digite o CEP..."
+                            />
+                        </div>
+                    </div>
+                    
+                </div>
+                <div className="row">
+                    <div className="col-12 d-flex justify-content-end">
+                        <button className="btn btn-primary m-1"
+                            onClick={e => this.save(e)}>
+                            Salvar
+                        </button>
+                        <button className="btn btn-secondary m-1"
+                            onClick={e => this.clear(e)}>
+                            Cancelar
+                        </button>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+
 
 
 
@@ -88,10 +232,12 @@ export default class CustomerCrud extends Component {
                     <td>{customer.estado}</td>
                     <td>{customer.cep}</td>
                     <td>
-                        <button className="btn btn-warning">
+                        <button className="btn btn-warning"
+                            onClick={() =>this.load(customer)}>
                             <i className="fa fa-pencil"></i>
                         </button>
-                        <button className="btn btn-danger ml-2">
+                        <button className="btn btn-danger ml-2"
+                            onClick={() =>this.remove(customer)}>
                             <i className="fa fa-trash"></i>
                         </button>
                     </td>
@@ -102,6 +248,7 @@ export default class CustomerCrud extends Component {
     render() {
         return (
             <Main {...headerProps}>
+                {this.renderForm()}
                 {this.renderTable()}
             </Main>
         )
