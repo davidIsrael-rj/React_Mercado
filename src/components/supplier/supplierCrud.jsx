9ca79ca83b9003989b2349e3b9c2a9cd908055f9
DavidIsrael <davidisrael.rj@gmail.com>
2025-01-25 10:11:56 -0300

    Inserindo o Formuilário

--- src/components/supplier/supplierCrud.jsx
+++ src/components/supplier/supplierCrud.jsx
@@ -61,6 +61,115 @@ export default class SupplierCrud extends Component {
         this.setState({supplier})
     }
     
+    renderForm(){
+        return(
+            <div className="form">
+                <div className="row">
+                    <div className="col-12 col-md-6">
+                        <div className="form-group">
+                            <label>Nome</label>
+                            <input type="text" className="form-control" 
+                                name="name"
+                                value={this.state.supplier.name}
+                                onChange={e => this.updateField(e)}
+                                placeholder="Digite o NOme do Fornecedor"
+                            />
+                        </div>
+                    </div>
+                    <div className="col-12 col-md-6">
+                        <div className="form-group">
+                            <label>Endereço</label>
+                            <input type="text" className="form-control" 
+                                name="endereco"
+                                value={this.state.supplier.endereco}
+                                onChange={e => this.updateField(e)}
+                                placeholder="Digite o Endereço"
+                            />
+                        </div>
+                    </div>
+                    <div className="col-12 col-md-6">
+                        <div className="form-group">
+                            <label>Numero</label>
+                            <input type="text" className="form-control" 
+                                name="numero"
+                                value={this.state.supplier.numero}
+                                onChange={e => this.updateField(e)}
+                                placeholder="Digite o Numero"
+                            />
+                        </div>
+                    </div>
+                    <div className="col-12 col-md-6">
+                        <div className="form-group">
+                            <label>Bairro</label>
+                            <input type="text" className="form-control" 
+                                name="bairro"
+                                value={this.state.supplier.bairro}
+                                onChange={e => this.updateField(e)}
+                                placeholder="Digite o Bairro"
+                            />
+                        </div>
+                    </div>
+                    <div className="col-12 col-md-6">
+                        <div className="form-group">
+                            <label>Cidade</label>
+                            <input type="text" className="form-control" 
+                                name="cidade"
+                                value={this.state.supplier.cidade}
+                                onChange={e => this.updateField(e)}
+                                placeholder="Digite a Cidade"
+                            />
+                        </div>
+                    </div>
+                    <div className="col-12 col-md-6">
+                        <div className="form-group">
+                            <label>Estado</label>
+                            <input type="text" className="form-control" 
+                                name="estado"
+                                value={this.state.supplier.estado}
+                                onChange={e => this.updateField(e)}
+                                placeholder="Digite o Estado"
+                            />
+                        </div>
+                    </div>
+                    <div className="col-12 col-md-6">
+                        <div className="form-group">
+                            <label>Inscrição Estadual</label>
+                            <input type="text" className="form-control" 
+                                name="ie"
+                                value={this.state.supplier.ie}
+                                onChange={e => this.updateField(e)}
+                                placeholder="Digite a Inscrição Estadual"
+                            />
+                        </div>
+                    </div>
+                    <div className="col-12 col-md-6">
+                        <div className="form-group">
+                            <label>CEP</label>
+                            <input type="text" className="form-control" 
+                                name="cep"
+                                value={this.state.supplier.cep}
+                                onChange={e => this.updateField(e)}
+                                placeholder="Digite o CEP"
+                            />
+                        </div>
+                    </div>
+                </div>
+                <hr />
+                <div className="row">
+                    <div className="col-12 d-flex justify-content-end">
+                        <button className="btn btn-primary m-1"
+                            onClick={e => this.save(e)}>
+                            Salvar
+                        </button>
+                        <button className="btn btn-secondary m-1"
+                            onClick={e => this.clear(e)}>
+                            Cancelar
+                        </button>
+                    </div>
+                </div>
+            </div>
+        )
+    }
 
     renderTable(){
         return(
@@ -114,6 +223,7 @@ export default class SupplierCrud extends Component {
     render() {
         return (
             <Main {...headerProps}>
+                {this.renderForm()}
                 {this.renderTable()}
             </Main>
         )
